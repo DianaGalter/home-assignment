9ce9d32efbf54da901a4ca0ab1c26f17e1a83ff8
DianaGalter <diana.bobrova.spb@gmail.com>
2023-07-12 00:57:29 +0300

    feat: add handlers to the dialog

--- src/components/ConfirmDialog/ConfirmDialog.tsx
+++ src/components/ConfirmDialog/ConfirmDialog.tsx
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Dialog, DialogActions, DialogContent, TextField } from '@mui/material';
 
 type ConfirmDialogProps = {
     open: boolean;
+    defaultValue?: string;
     onClose: () => void;
-    onSave: () => void;
+    onSave: (value: string) => void;
 }
 
-export const ConfirmDialog: React.FC<ConfirmDialogProps> = ({ open, onClose, onSave }) => {
+export const ConfirmDialog: React.FC<ConfirmDialogProps> = ({ open, defaultValue, onClose, onSave }) => {
+    const [value, setValue] = useState(defaultValue || '');
+    const [error, setError] = useState(false);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+        setError(false);
+    };
+
+    const onCancel = () => {
+        onClose();
+        setError(false);
+    };
+
+    const handleSave = () => {
+        if (value.trim() === '') {
+            setError(true);
+        } else {
+            onSave(value);
+            onClose();
+        }
+    };
 
     return (
         <Dialog open={open} onClose={onClose}>
             <DialogContent>
-                <TextField />
+                <TextField
+                    autoFocus
+                    label="Some text..."
+                    fullWidth
+                    variant="outlined"
+                    value={value}
+                    onChange={handleChange}
+                    error={error}
+                    helperText={error ? 'Text must not be blank' : ''}
+                />
             </DialogContent>
             <DialogActions>
-                <Button onClick={onClose} variant="text">Cancel</Button>
-                <Button onClick={onSave} variant="text">Save</Button>
+                <Button onClick={onCancel} variant="text">Cancel</Button>
+                <Button onClick={handleSave} variant="text">Save</Button>
             </DialogActions>
         </Dialog>
     );
